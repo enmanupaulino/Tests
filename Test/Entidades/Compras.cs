cc72740f546bc426bffc3d151bbbfc2302a7ccc0
Emmanuel <Emmanuel@DESKTOP-B6P5QMH>
2019-06-21 02:06:59 -0400

    Agregando Entidades

---
+++ Test/Entidades/Compras.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Entidades
+{
+    public class Compras
+    {
+        [Key]
+        public int IdCompro { get; set; }
+        public int IdUsuarios { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public int IdProveedor { get; set; }
+        public int IdComprobante { get; set; }
+        public double CostoCompra { get; set; }
+        public Compras()
+        {
+            IdCompro = 0;
+            IdUsuarios = 0;
+            FechaCompra = DateTime.Now;
+            IdProveedor = 0;
+            IdComprobante = 0;
+            CostoCompra = 0;
+        }
+    }
+}
---
+++ Test/Entidades/TiposComprobantes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Entidades
+{
+   public  class TiposComprobantes
+    {
+        [Key]
+        public int IdComprobante { get; set; }
+        public string  NombreComprobante { get; set; }
+        public TiposComprobantes(){
+            IdComprobante = 0;
+            NombreComprobante = string.Empty;
+        }
+    }
+}
