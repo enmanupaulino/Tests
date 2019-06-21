b5d3fbdc269d2cdd89ab7466f7c6ded1ac1e0cf9
Emmanuel <Emmanuel@DESKTOP-B6P5QMH>
2019-06-21 01:16:10 -0400

    Agregando Entidades

---
+++ Test/Entidades/Cargos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Entidades
+{
+    public class Cargos
+    {
+        [Key]
+        public int Idcargo { get; set; }
+        public string NombreCargo { get; set; }
+
+        public Cargos()
+        {
+            Idcargo = 0;
+            NombreCargo = string.Empty;
+        }
+    }
+}
---
+++ Test/Entidades/Personas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Entidades
+{
+  public class Personas
+    {
+        [Key]
+        public int IdPersona { get; set; }
+        public int dni { get; set; }
+        public string nombre { get; set; }
+        public string Paterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Sexo { get; set; }
+        public string Direccion { get; set; }
+
+
+        public Personas()
+        {
+            IdPersona = 0;
+            dni = 0;
+            nombre = string.Empty;
+            Paterno = string.Empty;
+            FechaNacimiento = DateTime.Now;
+            Telefono = string.Empty;
+            Correo = string.Empty;
+            Sexo = string.Empty;
+            Direccion = string.Empty;
+        }
+
+    }
+
+}
